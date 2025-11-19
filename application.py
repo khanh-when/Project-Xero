aab862f3947a2ccf1bae5d1f8a6898ca8a139c0d
khanh-when <tony.tnn.nguyen@gmail.com>
2025-11-19 01:11:20 -0600

    Insert Stock Function / Database Function Successful
    Next, insert all Ticker data into the Database,
    
    Then repeat for PriceDatas

--- application.py
+++ application.py
@@ -12,23 +12,45 @@ def connection(db_name):
                 password = '2123<>69',
                 host = 'localhost',
                 port = 3306,
-                database = db_name
+                database = db_name,
+                autocommit = True
             )
+    
     except mariadb.Error as e:
         print(f"Error: MariaDB Connection {e}")
+        raise mariadb.Error
+
+
+def insert_stock(conn, stock_id: str, security_name: str, listing_exchange: str, marketing_category: str, is_etf: bool):
+    '''Insert a New Stock into the Stocks Table'''
+    query = "INSERT INTO Stocks(Stock_ID, SecurityName, ListingExchange, MarketCategory, ETF) VALUES (?, ?, ?, ?, ?);"
+
+    try:
+        with conn.cursor() as cur:
+            cur.execute(query, (stock_id, security_name, listing_exchange, marketing_category, is_etf))
+        print(f"Stock: {stock_id} is successfully added to the Stock Table.")
+
+    except mariadb.Error as e:
+        print(f"Error: Insert - {e}")
         raise mariadb.Error
 
 
 def main():
 
     stocksData = stock_data()
-    pricesData = price_data('archive/stocks/*.csv')
-    
-    print(stocksData['AMD'])
-    print(pricesData['AMD']['2020-04-01'])
+
+    # print(stocksData['AMD']['SecurityName'])
+
+    x = stocksData['AMD']
+
+    # pricesData = price_data('archive/stocks/*.csv')
+    # print(stocksData['AMD'])
+    # print(pricesData['AMD']['2020-04-01'])
 
     try:
         conn = connection('stock_market')
+
+        insert_stock(conn, x['StockID'], x['SecurityName'], x['ListingExchange'], x['MarketCategory'], x['ETF'])
 
     except Exception as e:
         print(f'Error: {e}')
